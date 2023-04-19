e811bd9825ff286e4be1e3ec72c52d8571c558d0
Litrus <jasonq578@gmail.com>
2023-04-20 11:00:33 +1200

    branch test

--- bot.py
+++ bot.py
@@ -12,14 +12,13 @@ print("Loading tokens..")
 
 def run_discord_bot():
     print("Bot waking up...")
-    TOKEN = os.getenv('DISCORD_TOKEN')
     intents = discord.Intents.all()
     intents.message_content = True
     bot = commands.Bot(command_prefix='/', intents=intents)
 
     @bot.event
     async def on_ready():
-        print(f'{bot.user} is now running!')
+        print(f'{bot.user} is online')
         try:
             synced = await bot.tree.sync()
             print(f"Synced {len(synced)} command(s)")
@@ -35,16 +34,16 @@ def run_discord_bot():
     async def weather(interaction: discord.Interaction, location: str):
         API_KEY = os.getenv("WEATHER_API_KEY")
         try:
-            url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}'  # takes a city and api key
+            url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}'  #Leaving api_key as ver to be more readable
             print(url)
             response = requests.get(url)  # fetches the data with a http request using the formatted url(json format)
             data = json.loads(response.text)  # creates a dictionary from the response JSON returned
             weather = data['weather'][0]['description']
             country = data['sys']['country']
-            temperature = round(float(data['main']['temp']) - 273.15)  # kelvin to c
+            temperature = round(float(data['main']['temp']) - 273.15)  #kelvin to c
             feelslike = round(float(data['main']['feels_like']) - 273.15)
             humidity = data['main']['humidity']
-            message = f"The weather in {country}, {location} is a/an {weather}. \nThe temperature is {temperature}\N{DEGREE SIGN}C and feels like {feelslike}\N{DEGREE SIGN}C. \nThe humidity is {humidity}%."
+            message = f"The weather in {country}, {location} is {weather}. \nThe temperature is {temperature}\N{DEGREE SIGN}C and feels like {feelslike}\N{DEGREE SIGN}C. \nThe humidity is {humidity}%."
             print("Fetched successfully: ", response)
             await interaction.response.send_message(message)
         except Exception as e:
@@ -62,4 +61,20 @@ def run_discord_bot():
         channel = str(after.channel)
         print(f"{username} edited a message in '{channel}. Prior to edit: '{prev}' after edit: '{post}'")
 
-    bot.run(TOKEN)
+    bot.run(os.getenv('DISCORD_TOKEN'))
+
+    '''TO DO
+    Learn how to github branch so i dont have to ctrl z anymore. - tonight if possible
+    
+    Remove main, run bot from just this one python file
+    
+    Figure out cogs, create a voice module that will control voice channel activities such as: join, leave and audio processing such as
+    reading/playing audio files. Incorporate spotipy and play a song from a playlist.
+    
+    Maybe: api cog for any api calls if adding more api commands.
+    
+    Daily checklist that spawns at x time. should have: checklist with items that can be ticked and unticked,
+    a submission button to acknowledge that everything is done for the day. Create a database to store
+    finished tasks, etc.
+    
+    '''
