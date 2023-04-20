c4d7c6db8cf0209f47c0c2ee5ebbbac7c58ca27d
Litrus <jasonq578@gmail.com>
2023-04-20 22:17:48 +1200

    File overhaul and cog separation

--- bot.py
+++ bot.py
@@ -1,78 +1,49 @@
 import discord
 import os
 import asyncio
-import requests
-import json
 from discord.ext import commands
-from discord import app_commands
 from dotenv import load_dotenv
 
-load_dotenv()
-print("Loading tokens..")
+class myBot(commands.Bot):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(command_prefix = '/', intents = intents, help_command=None, )
 
-def main():
-    print("Bot waking up...")
-    intents = discord.Intents.all()
-    intents.message_content = True
-    bot = commands.Bot(command_prefix='/', intents=intents)
+    async def setup_hook(self) -> None:
+        await self.load_extension('cog.apiFunctions')
+    async def close(self) -> None:
+        await super().close()
 
-    @bot.event
-    async def on_ready():
-        print(f'{bot.user} is online')
-        try:
-            synced = await bot.tree.sync()
-            print(f"Synced {len(synced)} command(s)")
-        except Exception as e:
-            print(e)
+async def main():
+    load_dotenv()
+    print("Loading tokens in main..")
 
-    @bot.tree.command(name = "hello")
-    async def hello(interaction: discord.Interaction):
-        await interaction.response.send_message(f"Hello {interaction.user.mention}!")
+    async with myBot(intents = discord.Intents.all()) as bot:
+        @bot.event
+        async def on_ready():
+            print(f'{bot.user} is online')
+            try:
+                synced = await bot.tree.sync()
+                print(f"Synced {len(synced)} command(s)")
+            except Exception as e:
+                print(e)
 
-    @bot.tree.command(name = "weather")
-    @app_commands.describe(location = "City to check")
-    async def weather(interaction: discord.Interaction, location: str):
-        API_KEY = os.getenv("WEATHER_API_KEY")
-        try:
-            url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}'  #Leaving api_key as ver to be more readable
-            print(url)
-            response = requests.get(url)  # fetches the data with a http request using the formatted url(json format)
-            data = json.loads(response.text)  # creates a dictionary from the response JSON returned
-            weather = data['weather'][0]['description']
-            country = data['sys']['country']
-            temperature = round(float(data['main']['temp']) - 273.15)  #kelvin to c
-            feelslike = round(float(data['main']['feels_like']) - 273.15)
-            humidity = data['main']['humidity']
-            message = f"The weather in {country}, {location} is {weather}. \nThe temperature is {temperature}\N{DEGREE SIGN}C and feels like {feelslike}\N{DEGREE SIGN}C. \nThe humidity is {humidity}%."
-            print("Fetched successfully: ", response)
-            await interaction.response.send_message(message)
-        except Exception as e:
-            print("Unsuccessful fetch: invalid location, API key is not working or openweather API is down.")
+        #Command to reload every cog
+        @commands.is_owner()
+        @bot.tree.command(name = "reload")
+        async def reload_cogs(interaction: discord.Interaction):
+            print("Reloading cogs...")
+            cogs = list(bot.extensions.keys())
+            for cog in cogs:
+                await bot.reload_extension(cog)
+                print(f"User {interaction.user} has reloaded {cog} ")
+            await interaction.response.send_message("Reloaded all cogs", ephemeral=True)
 
-    # showing edited messages
-    @bot.event
-    async def on_message_edit(before, after):
-        if after.author == bot.user:
-            return
+        @bot.tree.command(name = "hello")
+        async def hello(interaction: discord.Interaction):
+            await interaction.response.send_message(f"Hello {interaction.user.mention}!")
 
-        username = str(after.author)
-        prev = before.content
-        post = after.content
-        channel = str(after.channel)
-        print(f"{username} edited a message in '{channel}. Prior to edit: '{prev}' after edit: '{post}'")
+        await bot.start(os.getenv('DISCORD_TOKEN'))#Accesses default aenter method of the bot class
 
-    bot.run(os.getenv('DISCORD_TOKEN'))
 
-asyncio.run(main(), debug=True)
 
-'''TO DO
-    Figure out cogs, create a voice module that will control voice channel activities such as: join, leave and audio processing such as
-    reading/playing audio files. Incorporate spotipy and play a song from a playlist.
-    
-    Maybe: api cog for any api calls if adding more api commands.
-    
-    Daily checklist that spawns at x time. should have: checklist with items that can be ticked and unticked,
-    a submission button to acknowledge that everything is done for the day. Create a database to store
-    finished tasks, etc.
-    
-    '''
+asyncio.run(main(), debug = True)
---
+++ cog/apiFunctions.py
@@ -0,0 +1,39 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import requests
+import json
+from dotenv import load_dotenv
+import os
+class apiFunctions(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
+        print("API Functions loaded")
+
+    load_dotenv()
+    print("Loading tokens in apiFunctions..")
+
+    @app_commands.command(name = "weather", description="Enter location/city")
+    async def weather(self, interaction: discord.Interaction, location: str):
+        API_KEY = os.getenv("WEATHER_API_KEY")
+        try:
+            url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}'  #Leaving api_key as ver to be more readable
+            print(url)
+            response = requests.get(url)  # fetches the data with a http request using the formatted url(json format)
+            data = json.loads(response.text)  # creates a dictionary from the response JSON returned
+            weather = data['weather'][0]['description']
+            country = data['sys']['country']
+            temperature = round(float(data['main']['temp']) - 273.15)  #kelvin to c
+            feelslike = round(float(data['main']['feels_like']) - 273.15)
+            humidity = data['main']['humidity']
+            message = f"The weather in {country}, {location} is {weather}. \nThe temperature is {temperature}\N{DEGREE SIGN}C and feels like {feelslike}\N{DEGREE SIGN}C. \nThe humidity is {humidity}%."
+            print("Fetched successfully: ", response)
+            await interaction.response.send_message(message)
+        except Exception as e:
+            print("Unsuccessful fetch: invalid location, API key is not working or openweather API is down.")
+
+async def setup(bot):
+    await bot.add_cog(apiFunctions(bot))
+
+async def teardown(bot):
+    await bot.remove_cog(apiFunctions(bot))
